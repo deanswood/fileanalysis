5ce9ae7ca4203b6814deb44f4769b1893e408b83
Dean Wood <deanwood@c02nw0s5g3qd.home>
2015-06-07 19:56:22 +0100

    Updated comments and slight restructure to avoid extracting characters twice

--- src/main/scala/fileStats.scala
+++ src/main/scala/fileStats.scala
@@ -4,13 +4,16 @@ import scala.io.Source
 
 object Filestats{
 
+	// Initially, define utilities used later in the programme.
 	// Define Set containing letters to count. This will avoid the character count
-	// including punctuation
+	// including punctuation.
 	val ordinary: Set[Char] = (('a' to 'z') ++ ('A' to 'Z') ++ ('0' to '9')).toSet
+
 	// Define function that strips out special characters.
 	def isOrdinary(s: String) = s.forall(ordinary.contains(_))
 
-	//Line Count
+	// Function for carrying out the Line Count.
+	// Should be called with a file source.
 	def countLines2(source: Source): Long = {
 		var newlineCount = 0L
 		for (line <- source.getLines()) {
@@ -20,53 +23,63 @@ object Filestats{
 	}
 
 	def main(args: Array[String]) {
-		var words = 0
-		val counts = mutable.Map[String, Int]().withDefault(x => 0)
+		// Variables for carrying out calculations.
+		// Variable for word count.
+		var wordcount = 0
+		// Mutable map for holding the words used with their frequencies. 
+		val words = mutable.Map[String, Int]().withDefault(x => 0)
+		// Mutable map for holding letter frequencies.
 		val letterCounts = mutable.Map[String, Int]().withDefault(x => 0)
 		val filename = readLine("Please enter the full path and filename of the file to be read in.")
 		val logData = Source.fromFile(filename)
 
-		// Aim to minimise IO. Will extract word count, number of times
-		// words are used and letter count. From this, many other stats can be calculated.
-		// Split the lines on empty space using regex
+		// Loop to extract word count, words and their frequencies and letter frequencies.
+		// Holding these in variables rather than discarding them should allow further statistics
+		// to be calculated if required.
+		// Split the lines on empty space using regex.
 		for (token <- logData.getLines().flatMap(x => x.split("\\s+"))) {
 			// Ignore empty lines which appear in the word count as ""
 			if (token != "") {
-				words += 1
-				counts(token) += 1
-				for (letters <- token.split("")) {
-					if (isOrdinary(letters)) {
-						letterCounts(letters) += 1
-					}
-				}
+				// Increment word count.
+				wordcount += 1
+				// Insert word into map if doesn't exist and increment frequency
+				words(token) += 1
 			}
 		}
+		// Move to immutable storage
+		val fixedWordCount = wordcount
 
 		// Set two variables to hold the number of letters per word and running total of word length.
-		var lettercount = 0
+		var tempLetterCount=0
 		var avgword=0
-		for((key, value) <- counts){
+		for((key, value) <- words){
 			for(wordlengths <- key.split("")){
 				if (isOrdinary(wordlengths)) {
-					lettercount +=1
+					tempLetterCount +=1
+					letterCounts(wordlengths) += value
 				}
-				avgword += lettercount*value
-				lettercount = 0
+				avgword += tempLetterCount*value
+				tempLetterCount = 0
 			}
 		}
+		// Copy letterCounts to immutable map
+		val fixedLetterCounts = letterCounts.toMap
 
 		// Move mutable variables to immutable for safety.
-		val fixedWords = words
 		// Finish calculating the average word length so it is immutable.
-		val fixedAvgWord=avgword/fixedWords
-    // Carry out the line count and save the result to immutable storage.
+		val fixedAvgWord=avgword/fixedWordCount
+    		// Carry out the line count and save the result to immutable storage.
+		// The iterator will be empty having read through the file once so must be reinitialised.
 		val logData2 = Source.fromFile(filename)
+		// TODO combine this line count with the first time through the file
 		val fixedLines = countLines2(logData2)
-
-		val fixedLetterCounts = letterCounts.toMap
+		
+		// Finally, extract the letter with the highest frequency
 		val maxLetterCounts = fixedLetterCounts.values.max
 		val highestLetter = letterCounts.filter((t) => t._2 == maxLetterCounts)
-		print("Word Count ="+fixedWords+"\n")
+
+		// Print out the values.
+		print("Word Count ="+fixedWordCount+"\n")
 		print("Line Count = "+fixedLines+"\n")
 		print("Most Common Letter(s)\n")
 		highestLetter.foreach((t2) => println (t2._1 + ", " + t2._2 + " times \n"))
